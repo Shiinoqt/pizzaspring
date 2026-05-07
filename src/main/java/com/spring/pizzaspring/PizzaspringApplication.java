d06f0c74d147e5f6c9543523647303c196c1c833
shiinoqt <damienrebong@outlook.com>
2026-05-07 10:55:40 +0200

    Initial commit for PizzaSpring project setup with Spring Boot. Added Maven wrapper and configuration files. Created main application class, application properties, and test class. Included necessary dependencies for Spring Data JPA and MySQL connector.

---
+++ src/main/java/com/spring/pizzaspring/PizzaspringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.pizzaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/pizzaspring/PizzaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.pizzaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
